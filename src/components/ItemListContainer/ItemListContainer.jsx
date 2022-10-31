b91c7bcd68df4c9e91315756efd06033f8635e0c
NachoPolinari <nachopolinari@gmail.com>
2022-10-31 14:32:24 -0300

    le intento agregar productos

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import { useEffect } from 'react'
+import gFetch from '../helpers/gFetch'
 
-const ItemListContainer = ({greeting,titulo}) => {
+const ItemListContainer = ({ greeting, titulo }) => {
+
+  useEffect(() => {
+    gFetch()
+      .then(resSgte => console.log(resSgte))
+      .catch(err => console.log(err))
+      .finally(() => console.log('finalizo la prmesa'))
+  }, [])
+
   return (
     <div>
       {greeting}
       {titulo}
+
+
     </div>
   )
 }
