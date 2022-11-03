80fab0c5b66d208a721329c5055edc610bd9a672
NachoPolinari <nachopolinari@gmail.com>
2022-11-03 20:53:14 -0300

    routing y 404

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,33 @@
 import { useState } from 'react'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 
 import ItemListContainer from './components/pages/ItemListContainer/ItemListContainer'
 import Navbar from './components/Navbar/Navbar'
-
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
 import ItemDetailContainer from './components/pages/ItemDetailContainer/ItemDetailContainer';
+import CartPages from './components/pages/cartPages/cartPages';
+import NotFound404 from './components/NotFound404/NotFound404';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
 
 
 function App() {
 
     return (
-        <div className="App">
+        <BrowserRouter>
+
             <Navbar />
-            <ItemListContainer greeting= {'Hello World'} titulo={'item List Container'}/>
-            <ItemDetailContainer/>
-        </div>
+            <Routes>
+                <Route path='/' element={<ItemListContainer greeting={'Hello World'} titulo={'item List Container'} />}/>
+                <Route path='/detalle' element={<ItemDetailContainer />}/>
+                <Route path='/cart' element={<CartPages /> }/>
+                <Route path='/404' element={<NotFound404/>} />
+
+
+                <Route path='*' element={<Navigate to='/404'/>} />
+            </Routes>
+        </BrowserRouter>
+
     )
 }
 
---
+++ src/components/NotFound404/NotFound404.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NotFound404 = () => {
+  return (
+    <div>Error 404 - NOT FOUND</div>
+  )
+}
+
+export default NotFound404
